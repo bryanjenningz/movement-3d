73baeda15a1d3497675b8f4f10cc441b5f2597f4
Bryan Jennings <bryanjenningz@gmail.com>
2022-11-03 22:22:51 -0700

    Add GroundItem

--- src/Item.elm
+++ src/Item.elm
@@ -1,5 +1,14 @@
-module Item exposing (Item(..))
+module Item exposing (GroundItem, Item(..))
+
+import Length exposing (Meters)
+import Point3d exposing (Point3d)
 
 
 type Item
     = Coins Int
+
+
+type alias GroundItem =
+    { item : Item
+    , location : Point3d Meters Meters
+    }
